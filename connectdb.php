b1aecc94f0e2202e2bcb8398e751b45def2a449e
NYY <61612345+yni58-ni@users.noreply.github.com>
2022-03-27 23:59:40 -0400

    Update connectdb.php

--- connectdb.php
+++ connectdb.php
@@ -1,11 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
 <?php
-$dbhost = "localhost";
+$dbhost = "MacBook-Pro.local";
 $dbuser= "root";
-$dbpass = "a!438589185@L";
-$dbname = "live";
+$dbpass = "";
+$dbname = "sensor_data";
 $connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname); 
+$query = 'SELECT * FROM data';
 if (mysqli_connect_errno()) {
-	die("Database connection failed:".
-	mysqli_connection_error()."(".mysqli_connect_errno().")");
+	echo "Failed to connect to MySQL: " . mysqli_connect_error();
 }
-?>
+$result = mysqli_query($connection,$query);
+if (!$result) {
+	die("database query failed.");
+  }
+echo "<table border='1'>";
+
+while($row = mysqli_fetch_assoc($result))
+{
+echo "<tr>";
+echo "<td>" . $row['id'] . "</td>";
+echo "<td>" . $row['time'] . "</td>";
+echo "<td>" . $row['value'] . "</td>";
+echo "</tr>";
+}
+echo "</table>";
+
+mysqli_close($connection);
+?> 
+	
+</body>
+</html>

1d221201c8bea884bbea644656343d304033f4ee
BerthaCoghlin <jarviscoghlin@gmail.com>
2022-04-23 19:50:28 -0400

    Changed files

--- connectdb.php
+++ connectdb.php
@@ -10,27 +10,46 @@ $dbpass = "";
 $dbname = "sensor_data";
 $connection = mysqli_connect($dbhost, $dbuser,$dbpass,$dbname); 
 $query = 'SELECT * FROM data';
-if (mysqli_connect_errno()) {
-	echo "Failed to connect to MySQL: " . mysqli_connect_error();
-}
-$result = mysqli_query($connection,$query);
-if (!$result) {
-	die("database query failed.");
-  }
-echo "<table border='1'>";
 
-while($row = mysqli_fetch_assoc($result))
-{
-echo "<tr>";
-echo "<td>" . $row['id'] . "</td>";
-echo "<td>" . $row['time'] . "</td>";
-echo "<td>" . $row['value'] . "</td>";
-echo "</tr>";
-}
-echo "</table>";
+function test(){
 
-mysqli_close($connection);
-?> 
+	echo array(4,5,6);
+
+}
+
+
+function get_data(){
+	
+	echo "<table border='1'>";
+	while($row = mysqli_fetch_assoc($result))
+	{
+	echo "<tr>";
+	echo "<td>" . $row['id'] . "</td>";
+	echo "<td>" . $row['time'] . "</td>";
+	echo "<td>" . $row['value'] . "</td>";
+	echo "</tr>";
+	}
+	echo "</table>";
+
+	return 1;
+
+}
+
+	if (mysqli_connect_errno()) {
+		echo "Failed to connect to MySQL: " . mysqli_connect_error();
+	}
+	$result = mysqli_query($connection,$query);
+	if (!$result) {
+		die("database query failed.");
+	}
+
+	get_data();	
+
+	mysqli_close($connection);
+	?> 
+
 	
 </body>
 </html>
+
+return 1;
